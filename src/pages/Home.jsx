939e88c6708d3f35fc35916476f869169c40a784
0TKA3 <derakrun@gmail.com>
2024-01-02 17:28:00 +0300

    fetch request replaced by axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,8 @@ import Sort from "../components/Sort";
 import Categories from "../components/Categories";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
 
 export default function Home() {
   const categoryId = useSelector(state => state.filter.categoryId)
@@ -22,17 +23,12 @@ export default function Home() {
 
   useEffect(() => {
     setIsLoading(true);
-    fetch(
-      "https://658ee58f2871a9866e79ff4c.mockapi.io/items" +
-        `?sortBy=${sortList[sortType]}&order=${sortOrder}` +
-        (categoryId ? `&category=${categoryId}` : "") +
-        (searchValue ? `&search=${searchValue}` : "")
-    )
+    axios.get("https://658ee58f2871a9866e79ff4c.mockapi.io/items" +
+      `?sortBy=${sortList[sortType]}&order=${sortOrder}` +
+      (categoryId ? `&category=${categoryId}` : "") +
+      (searchValue ? `&search=${searchValue}` : ""))
       .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setItems(json);
+        setItems(res.data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
@@ -42,16 +38,16 @@ export default function Home() {
     <>
       <div className="container">
         <div className="content__top">
-          <Categories/>
-          <Sort/>
+          <Categories />
+          <Sort />
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
           {isLoading
             ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
             : items
-                .filter((obj) => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
-                .map((obj, index) => <PizzaBlock {...obj} key={index}></PizzaBlock>)}
+              .filter((obj) => obj.title.toLowerCase().includes(searchValue.toLowerCase()))
+              .map((obj, index) => <PizzaBlock {...obj} key={index}></PizzaBlock>)}
         </div>
       </div>
     </>
