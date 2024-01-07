4688c36f3099afcb6a0141ee7f6b30d5c96b2c5a
0TKA3 <derakrun@gmail.com>
2024-01-07 18:28:53 +0300

    router fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import {store} from './redux/store'
+import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
