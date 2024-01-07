69ffcef6396c87451b47a16bb3d17dad1e5cf862
0TKA3 <derakrun@gmail.com>
2024-01-07 18:35:16 +0300

    router fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
-import { HashRouter as BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
